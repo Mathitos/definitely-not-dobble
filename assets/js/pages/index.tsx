3b1f322c745a38f82c64ac5a5aa2b816cab9632f
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-12-09 17:45:47 -0300

    get values from queryparams

--- assets/js/pages/index.tsx
+++ assets/js/pages/index.tsx
@@ -2,13 +2,10 @@ import React from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 import Chat from '../components/Chat'
 
-const HomePage: React.FC<RouteComponentProps> = (props) => {
-  return (
-    <>
-      {console.log(props)}
-      <Chat name="matheus" chatRoom="chat" />
-    </>
-  )
+const HomePage: React.FC<RouteComponentProps> = ({ location: { search } }) => {
+  const params = new URLSearchParams(search)
+  const name = params.get('name')
+  return <Chat name={name} chatRoom="chat" />
 }
 
 export default HomePage
