8bd242a41fc6c587be05e4ae3cd4db78780aa580
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-12-11 16:51:34 -0300

    add describe to tests

--- test/definitely_not_dobble/dobble_test.exs
+++ test/definitely_not_dobble/dobble_test.exs
@@ -25,30 +25,43 @@ defmodule DefinitelyNotDobble.DobbleTest do
     %{user: @server_user, card: @server_card, cooldown: false}
   ]
 
-  test "get_current_cards" do
-    current_cards = get_current_cards(@game_state)
-    assert @server_card in current_cards
-    assert @user1_card in current_cards
-    assert @user2_card in current_cards
+  describe "get_current_cards/1" do
+    test "get_current_cards" do
+      current_cards = get_current_cards(@game_state)
+      assert @server_card in current_cards
+      assert @user1_card in current_cards
+      assert @user2_card in current_cards
+    end
   end
 
-  test "generate_new_card" do
-    new_card = generate_new_card(@game_state)
+  describe "generate_new_card/1" do
+    test "generate_new_card" do
+      new_card = generate_new_card(@game_state)
 
-    assert @server_card != new_card
-    assert @user1_card != new_card
-    assert @user2_card != new_card
+      assert @server_card != new_card
+      assert @user1_card != new_card
+      assert @user2_card != new_card
+    end
   end
 
-  test "add_new_player" do
-    game_state = [%{user: @server_user, card: @server_card, cooldown: false}]
-    new_game_state = add_new_player(@user1, game_state)
+  describe "add_new_player/2" do
+    test "add_new_player" do
+      game_state = [%{user: @server_user, card: @server_card, cooldown: false}]
+      new_game_state = add_new_player(@user1, game_state)
 
-    assert Enum.find(new_game_state, fn player -> player.user == @user1 end) != nil
+      assert Enum.find(new_game_state, fn player -> player.user == @user1 end) != nil
+    end
   end
 
-  test "guess" do
-    {result, _} = guess(@user1, 9, @game_state)
-    assert :wrong == result
+  describe "guess/3" do
+    test "wrong guess" do
+      {result, _} = guess(@user1, 17, @game_state)
+      assert :wrong == result
+    end
+
+    test "guessing a number server dont have" do
+      {result, _} = guess(@user1, 9, @game_state)
+      assert :wrong == result
+    end
   end
 end
