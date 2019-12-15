662434ba3ffaa0cedcdc29ee4f2deb76b8d484b9
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-12-15 20:10:38 -0300

    randomize card style

--- assets/js/components/DobbleGameCard/index.tsx
+++ assets/js/components/DobbleGameCard/index.tsx
@@ -8,11 +8,42 @@ const DobbleGameCard: React.FC<{ card: number[]; onGuess?: (guess: number) => vo
 }) => (
   <div className="dobble-game-card__container">
     {card.map((number) => (
-      <button className="dobble-game-card__button" onClick={() => onGuess && onGuess(number)}>
+      <button
+        className="dobble-game-card__button"
+        style={getNumberStyle(number)}
+        onClick={() => onGuess && onGuess(number)}
+      >
         {number}
       </button>
     ))}
   </div>
 )
+
+const colors: string[] = [
+  'Lavender',
+  'YellowGreen',
+  'Teal',
+  'SaddleBrown',
+  'Salmon',
+  'RebeccaPurple',
+  'Red',
+  'Magenta',
+]
+
+const getNumberColor = (number: number): string => colors[number % 8]
+
+const getNumberRotation = (): string => `rotate(${90 - Math.floor(Math.random() * 180)}deg)`
+
+const getNumberHeight = (number: number): 'normal' | 'bold' =>
+  number % 2 === 0 ? 'normal' : 'bold'
+
+const getFontSize = (): string => `${14 + Math.floor(Math.random() * 10)}px`
+
+const getNumberStyle = (number: number): React.CSSProperties => ({
+  color: getNumberColor(number),
+  fontSize: getFontSize(),
+  transform: getNumberRotation(),
+  fontWeight: getNumberHeight(number),
+})
 
 export default DobbleGameCard
