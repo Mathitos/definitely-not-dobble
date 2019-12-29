3361dd679bb3c9bf828341eacddc08b98f7ce923
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-12-29 13:31:30 -0300

    remove client logic and convert project to just an api

--- config/dev.exs
+++ config/dev.exs
@@ -11,23 +11,12 @@ config :definitely_not_dobble, DefinitelyNotDobble.Repo,
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with webpack to recompile .js and .css sources.
 config :definitely_not_dobble, DefinitelyNotDobbleWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [
-    {"node",
-     [
-       "node_modules/webpack/bin/webpack.js",
-       "--watch-stdin",
-       "--colors"
-     ]}
-  ]
+  watchers: []
 
 # ## SSL Support
 #
--- lib/definitely_not_dobble_web/router.ex
+++ lib/definitely_not_dobble_web/router.ex
@@ -1,25 +1,11 @@
 defmodule DefinitelyNotDobbleWeb.Router do
   use DefinitelyNotDobbleWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/api", DefinitelyNotDobbleWeb do
     pipe_through :api
-  end
-
-  scope "/", DefinitelyNotDobbleWeb do
-    pipe_through :browser
-
-    get "/*path", PageController, :index
   end
 end
